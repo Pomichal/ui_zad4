4cb5fc41e6a53775811aa41378fc2f78985c08ff
Pomichal <vajk.pomichal@gmail.com>
2018-05-01 14:04:10 +0200

    added json with basic rules

--- zad4.py
+++ zad4.py
@@ -1,9 +1,10 @@
+import json
 
+with open('rules.json') as data_file:
+    data = json.load(data_file)
+    row = data["rules"]
 
+print(row.get("rodic1"))
 
-
-# with open('config.json') as data_file:
-#     data = json.load(data_file)
-#     row = data["map"]["rows"]
 
 
